986accca9409ec97df972bf9dcb6cd8a314443ac
gilberthappi <gdushimimana6@gmail.com>
2024-02-12 19:35:24 +0200

    New deploy 1

---
+++ spec/factories/place.rb
@@ -0,0 +1,12 @@
+# spec/factories/place.rb
+FactoryBot.define do
+    factory :place do
+      # Define attributes here
+      owner_id { 2 }
+      description { "Test Description" }
+      location { "Test Location" }
+      rate { 10 }
+      # Add other attributes as needed
+    end
+  end
+  
