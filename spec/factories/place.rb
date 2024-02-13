6e2f66294eb0b670586013c7383e2d77265f2f61
Ivonne <ivonnebenites2015@gmail.com>
2024-02-12 23:42:51 -0500

    Update file

--- spec/factories/place.rb
+++ spec/factories/place.rb
@@ -1,12 +1,10 @@
-# spec/factories/place.rb
 FactoryBot.define do
-    factory :place do
-      # Define attributes here
-      owner_id { 2 }
-      description { "Test Description" }
-      location { "Test Location" }
-      rate { 10 }
-      # Add other attributes as needed
-    end
+  factory :place do
+    # Define attributes here
+    owner_id { 2 }
+    description { "Test Description" }
+    location { "Test Location" }
+    rate { 10 }
+    # Add other attributes as needed
   end
-  
+end
